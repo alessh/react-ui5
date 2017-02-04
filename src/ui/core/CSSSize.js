08a09d9345f99d40cf834827e1c7744855d3d499
Alessandro Holanda <alessandro.holanda@hotmail.com>
2017-02-04 16:28:56 -0200

    add CSSSize property type

--- src/ui/core/CSSSize.js
+++ src/ui/core/CSSSize.js
@@ -1,8 +1,13 @@
-import React, {PropTypes} from 'react'
-import classnames from 'classnames'
+import React from 'react'
 
-const CSSSize (props) => {
-	return ( null )
+const CSSSize = {
+	isValid : function(vValue) {
+		// Note: the following regexp by intention is a single regexp literal.
+		// It could be made much more readable by constructing it out of (reused) sub-expressions (strings)
+		// but this would not be parseable by the metamodel recovery tooling that is used inside SAP
+		return /^(auto|inherit|[-+]?(0*|([0-9]+|[0-9]*\.[0-9]+)([rR][eE][mM]|[eE][mM]|[eE][xX]|[pP][xX]|[cC][mM]|[mM][mM]|[iI][nN]|[pP][tT]|[pP][cC]|%))|calc\(\s*(\(\s*)*[-+]?(([0-9]+|[0-9]*\.[0-9]+)([rR][eE][mM]|[eE][mM]|[eE][xX]|[pP][xX]|[cC][mM]|[mM][mM]|[iI][nN]|[pP][tT]|[pP][cC]|%)?)(\s*(\)\s*)*(\s[-+]\s|[*\/])\s*(\(\s*)*([-+]?(([0-9]+|[0-9]*\.[0-9]+)([rR][eE][mM]|[eE][mM]|[eE][xX]|[pP][xX]|[cC][mM]|[mM][mM]|[iI][nN]|[pP][tT]|[pP][cC]|%)?)))*\s*(\)\s*)*\))$/.test(vValue);
+	},
+	React.PropTypes.string
 }
 
 export { CSSSize }
